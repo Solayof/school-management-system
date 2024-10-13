b877401a0366714da09c9e1532acdc8fb9d975a7
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2024-10-13 01:11:41 -0700

    update setup script

--- setups/setup.py
+++ setups/setup.py
@@ -1,6 +1,7 @@
 #/usr/bin/python3
 import subprocess
 
-#subprocess.run(["cd setups"], capture_output=True, text=True)
+
 subprocess.run("cat setups/db_setup.sql | mariadb -uroot -proot", shell=True, capture_output=True, text=True)
-subprocess.run(["python", "manage.py"], capture_output=True, text=True)
+subprocess.run(["python3", "manage.py"], capture_output=True, text=True)
+subprocess.run(["python3", "-m", "setups.setup"], capture_output=True, text=True)
