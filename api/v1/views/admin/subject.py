7287b3d3499eaa57376c819eba50d7e6acd6582b
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-01-15 13:36:06 +0100

    import classes

---
+++ api/v1/views/admin/subject.py
@@ -0,0 +1,22 @@
+"""subject endpoints
+"""
+from datetime import date
+from flasgger import swag_from
+from flask import abort, jsonify, request
+from sqlalchemy.orm.attributes import flag_modified
+from api.v1.views.admin import admin as admbp
+from models.customExcept import InvalidAdmin
+from models.cbt.examination import Examination
+from models.cbt.question import Question
+from models.cbt.response import Response
+from models.cbt.score import Score
+from models.portal.admin import Admin
+from models.portal.admission import Admission
+from models.portal.Class import Class
+from models.portal.course import Course
+from models.portal.department import Department
+from models.portal.parent import Parent
+from models.portal.student import Student
+from models.portal.subject import Subject
+from models.portal.teacher import Teacher
+from models.portal.user import User
