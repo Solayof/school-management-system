355ee3979cc0b6ec87f2a9fd8b0063ea5dcd9350
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-02-01 14:30:15 +0100

    implement cbt question routes

--- api/v1/views/cbt/question.py
+++ api/v1/views/cbt/question.py
@@ -4,7 +4,7 @@ from datetime import date
 from flasgger import swag_from
 from flask import abort, jsonify, request
 from sqlalchemy.orm.attributes import flag_modified
-from api.v1.views.admin import admin as admbp
+from api.v1.views.cbt import cbt
 from models.customExcept import InvalidAdmin
 from models.cbt.examination import Examination
 from models.cbt.option import Option
@@ -21,3 +21,26 @@ from models.portal.student import Student
 from models.portal.subject import Subject
 from models.portal.teacher import Teacher
 from models.portal.user import User
+
+@cbt.route("/questions/<exam_id>", methods=["GET"], strict_slashes=False)
+@swag_from('', methods=['GET'])
+def getOneQuestion(exam_id):
+# Get exam by id
+    question = Question.get(exam_id)
+    # very if question exist or was published
+    if question is None or question.was_published() is False:
+        # Not Found
+        abort(404)
+    return jsonify(question.to_dict())
+
+
+@cbt.route("/pastquestions/<exam_id>", methods=["GET"], strict_slashes=False)
+@swag_from('', methods=['GET'])
+def pastQuestion(exam_id):
+# Get exam by id
+    question = Question.get(exam_id)
+    # very if question exist or was published
+    if question is None or question.was_published() is False:
+        # Not Found
+        abort(404)
+    return jsonify(question.to_dict())
