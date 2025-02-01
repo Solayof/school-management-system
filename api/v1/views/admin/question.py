4a0731b699b78a9e24a0347847f66e8655d9320b
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-02-01 14:26:30 +0100

    implement question routes for admin

--- api/v1/views/admin/question.py
+++ api/v1/views/admin/question.py
@@ -20,3 +20,78 @@ from models.portal.student import Student
 from models.portal.subject import Subject
 from models.portal.teacher import Teacher
 from models.portal.user import User
+
+
+@admbp.route("/questions/<exam_id>", methods=["GET", "PUT", "DELETE"], strict_slashes=False)
+@swag_from('', methods=['GET'])
+def oneQuestion(exam_id):
+# Get exam by id
+    question = Question.get(exam_id)
+    # very if question exist or was published
+    if question is None:
+        # Not Found
+        abort(404)
+# GET Method
+    if request.method == "GET":
+        return jsonify(question.to_dict())
+    
+    admin = request.admin
+    if admin.privileges is None:
+        abort(401)
+# DWLETE Method
+    if request.method == "DELETE":
+        if admin.privileges.get("delete") is False:
+            abort(401)
+        question.delete()
+        return jsonify({}), 204
+# PUT Method
+    if admin.privileges.get("update") is False:
+        abort(401)
+    info = request.get_json(silent=True)
+    if info is None:
+        abort(400, "Not a JSON")
+
+    for k, v in info.items():
+        if k == "id":
+            continue
+        if hasattr(Question, k):
+            if k == "pub_date":
+                try:
+                    v = date.fromisoformat(v)
+                except ValueError:
+                    continue
+            setattr(question, k, v)
+    question.save()
+    return jsonify(question.to_dict()), 202
+
+
+@admbp.route("/questions", methods=["POST"], strict_slashes=False)
+@swag_from('', methods=['POST'])
+def createQuestion():
+    admin = request.admin
+    print("here")
+    if admin.privileges is None:
+        abort(401)
+    if admin.privileges.get("create") is False:
+        abort(401)
+    # Get the request data
+    info = request.get_json(silent=True)
+    if info is None:
+        abort(400, "Not a JSON")
+
+    if not info.get("content"):
+        abort(400, "Missing name")
+    if not info.get("mode"):
+        abort(400, "Missing mode")
+
+    question = Question()
+    for k, v in info.items():
+        if hasattr(Question, k):
+            if k == "pub_date":
+                try:
+                    v = date.fromisoformat(v)
+                except ValueError:
+                    continue
+            setattr(question, k, v)
+    question.save()
+    return jsonify(question.to_dict()), 201
