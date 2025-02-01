39e37acbe423672c774c535b816a460bcb67d3a1
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-02-01 14:33:59 +0100

    define to_dict method for option classs

--- models/cbt/option.py
+++ models/cbt/option.py
@@ -15,11 +15,11 @@ from models.portal.association import exam_questions_asso
 class Option(BaseModel, Base):
     __tablename__ = "options"
     extend_existing = True
-    A = Column(String(1024))
-    B = Column(String(1024))
-    C = Column(String(1024))
-    D = Column(String(1024))
-    E = Column(String(1024))
+    A = Column(Text)
+    B = Column(Text)
+    C = Column(Text)
+    D = Column(Text)
+    E = Column(Text)
     answer = Column(String(2))
     note = Column(Text)
     question_id = Column(String(36), ForeignKey("questions.id"))
@@ -37,3 +37,32 @@ class Option(BaseModel, Base):
         back_populates="option",
         uselist=True
     )
+
+    def to_dict(self):
+        """dictionary representation of class instance
+
+        Returns:
+            _type_: dict
+        """
+        # if self.question is None or self.question.was_published() is False:
+        #     return {}
+        new_dict = self.__dict__.copy()
+        new_dict.pop("_sa_instance_state", None)
+        new_dict.pop('question', None)
+        new_dict.pop('response', None)
+        if self.A:
+            new_dict["A"] = self.A
+        if self.B:
+            new_dict["B"] = self.B
+        if self.C:
+            new_dict["C"] = self.C
+        if self.D:
+            new_dict['D'] = self.D 
+        if self.E:
+            new_dict['E'] = self.E
+        new_dict["note"] =self.note
+        new_dict['answer'] = self.answer 
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        
+        return new_dict
