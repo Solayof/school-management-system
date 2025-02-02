45bbfcfc5f8666ea6b31b3ba6baa479cb4d26f12
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-02-02 14:05:36 +0100

    add title attribute to app component class

--- web_dynamics/src/app/app.component.ts
+++ web_dynamics/src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'web_dynamics';
+  title = 'Comprehensive High School';
 }
