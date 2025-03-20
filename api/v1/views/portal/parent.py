7adbd72f7c3984e4a52e79c926db74fc8ed2e3af
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-03-20 22:45:38 +0100

    update parent route view

--- api/v1/views/portal/parent.py
+++ api/v1/views/portal/parent.py
@@ -21,8 +21,8 @@ from models.portal.teacher import Teacher
 from models.portal.user import User
 
 
-@portal.route("/parents/<parent_id>", methods=["GET", "PUT", "DELETE"], strict_slashes=False)
-@swag_from('../documentations/portal/parent/parent_id.yml', methods=['GET', 'PUT', 'DELETE'])
+@portal.route("/parents/<parent_id>", methods=["GET"], strict_slashes=False)
+@swag_from('../documentations/portal/parent/parent_id.yml', methods=['GET'])
 def parent(parent_id=None):
     """retrieve parent wth given id i.e parent_id
     
@@ -44,61 +44,9 @@ def parent(parent_id=None):
     if request.method == "GET":
         # To retrieve parent
         return jsonify(parent.to_dict()), 200
-    
-    # Only teacher who is an admin can DELETE and PUT.
-    # The teacher must have admin privileges
-    user_id = request.current_user.id
-    # Get the teacher instance
-    teacher = Teacher.get(user_id)
-    if teacher is None:
-        #not a teacher, permission denied
-        abort(401)
-    if teacher.isAdmin() is False:
-        abort(401)
-    admin = Admin.query.filter(Admin.teacher_id==user_id).one_or_none()
-    if admin is None:
-        abort(401)
-    
-    if admin.privileges is None:
-        abort(401)
 
-# DELETE method
-    if request.method == "DELETE":
-        if admin.privileges.get("delete") is False:
-            abort(401)
-        parent.delete()
-        return jsonify({}), 204
-    
-    if admin.privileges.get("update") is False:
-        abort(401)
-    
-    
-    # PUT method
-    if admin.privileges.get("update") is False:
-            abort(401)
-    info = request.get_json(silent=True)
-    if info is None:
-        abort(400, "Not a JSON")
-    for k, v in info.items():
-        if k not in ["created_at", "id"] and hasattr(Parent, k):
-            if k == "dob":
-                try:
-                    v = date.fromisoformat(v)
-                except ValueError:
-                    return jsonify({"Error": f"wrong {k} date format"}), 400
-            if k =="username":
-                if User.query.filter_by(username=v).one_or_none():
-                    return jsonify({"Error": f"User with {v} exist"}), 400
-            if k =="email":
-                if User.query.filter_by(email=v).one_or_none():
-                    return jsonify({"Error": f"User with {v} exist"}), 400
-            setattr(parent, k, v)
-    parent.save()
-    return jsonify(parent.to_dict()), 202
-        
-
-@portal.route("/parents", methods=["GET", "POST"])
-@swag_from('../documentations/portal/parent/parents.yml', methods=['GET', 'POST'])
+@portal.route("/parents", methods=["GET"])
+@swag_from('../documentations/portal/parent/parents.yml', methods=['GET'])
 def parents():
     """parent route
         GET: get all parents default limit of 10 parent
@@ -125,50 +73,8 @@ def parents():
             "results": [parent.to_dict() for parent in parents.all()]
         }
         return jsonify(results), 200
-    # Only teacher who is an admin with creat privilege can POST.
-    # The teacher must have admin privileges
-    user_id = request.current_user.id
-    # Get the teacher instance
-    # print("here")
-    teacher = Teacher.get(user_id)
-    if teacher is None:
-        #not a teacher, permission denied
-        abort(401)
-    if teacher.isAdmin() is False:
-        abort(401)
-    admin = Admin.query.filter(Admin.teacher_id==user_id).one_or_none()
-    if admin is None:
-        abort(401)
-    
-    if admin.privileges is None:
-        abort(401)
-    if admin.privileges.get("create") is False:
-        abort(401)
-    info = request.get_json(silent=True)
-    if info is None:
-        abort(400, "Not a JSON")
-    
-    if not info.get("username"):
-        abort(400, "Missing username")
-    if User.query.filter_by(username=info.get("username")).one_or_none():
-        abort(400, f"User with username {info.get('username')} exist")
-    if not info.get("email"):
-        abort(400, "Missing email")
-    if User.query.filter_by(username=info.get("email")).one_or_none():
-        abort(400, f"User with email {info.get('email')} exist")
-    parent = Parent()
-    for k, v in info.items():
-        if k != "id" and hasattr(Parent, k):
-            if k == "dob":
-                try:
-                    v = date.fromisoformat(v)
-                except ValueError:
-                    continue
-            setattr(parent, k, v)
-    parent.save()
-    return jsonify(parent.to_dict()), 201
-    
-@portal.route("/parents/<parent_id>/children", methods=["GET", "POST"], strict_slashes=False)
+
+@portal.route("/parents/<parent_id>/children", methods=["GET"], strict_slashes=False)
 def children(parent_id=None):
     """retrieve parent wth given id i.e parent_id
     
@@ -220,53 +126,3 @@ def children(parent_id=None):
              for i in  range(offset, end)]
         ]
         return jsonify(children), 200
-# Create chiidren for the parent
-    # Only teacher who is an admin with creat privilege can POST.
-    # The teacher must have admin privileges
-    user_id = request.current_user.id
-    # Get the teacher instance
-    teacher = Teacher.get(user_id)
-    if teacher is None:
-        #not a teacher, permission denied
-        abort(401)
-    if teacher.isAdmin() is False:
-        abort(401)
-    admin = Admin.query.filter(Admin.teacher_id==user_id).one_or_none()
-    if admin is None:
-        abort(401)
-    
-    if admin.privileges is None:
-        abort(401)
-    if admin.privileges.get("create") is False:
-        abort(401)
-    info = request.get_json(silent=True)
-    if info is None:
-        abort(400, "Not a JSON")
-    if not info.get("arm"):
-        abort(400, "Missing arm")
-    if not info.get("username"):
-        abort(400, "Missing username")
-    if User.query.filter_by(username=info.get("username")).one_or_none():
-        abort(400, f"User with username {info.get('username')} exist")
-    if not info.get("email"):
-        abort(400, "Missing email")
-    if User.query.filter_by(email=info.get("email")).one_or_none():
-        abort(400, f"User with email {info.get('email')} exist")
-    if not info.get("admission_no"):
-        abort(400, "Missing admission no")
-    admission_no = info.get("admission_no")
-    if User.query.filter_by(admission_no=admission_no).one_or_none():
-        abort(400, f"User with {info.get('admission_no')} exist")
-    
-    student = Student()
-    for k, v in info.items():
-        if k != "id" and hasattr(Student, k):
-            if k == "dob":
-                try:
-                    v = date.fromisoformat(v)
-                except ValueError:
-                   continue  
-            setattr(student, k, v)
-    student.parent_id = parent.id
-    student.save()
-    return jsonify(student.to_dict()), 201
