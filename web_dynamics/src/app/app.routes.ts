47738fc43165b80bc3d6e805d501cfb70ed82daa
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-03-20 22:48:52 +0100

    update app.route file

--- web_dynamics/src/app/app.routes.ts
+++ web_dynamics/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
     {path: 'login', title: "Login Page", component: LoginComponent},
-    {path: '', redirectTo: 'login', pathMatch: 'full'}
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: 'portal', title: "Portal", component: DashboardComponent}
 ];
