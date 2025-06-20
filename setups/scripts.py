2075a43d9bc1dbbbafb84fc7fdd06617319d5afd
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-06-21 00:28:25 +0100

    tracing bug

--- setups/scripts.py
+++ setups/scripts.py
@@ -93,79 +93,79 @@ print(GREEN +
 print(GREEN + 
       f"--{database.upper()} SUCCESSFULLY POPULATED--" + RESET)  
 
-def creatadmin():
-    teacher = Teacher(
-        username="superadmin",
-        email="superadmin@school.com",
-        firstName=fake.first_name(),
-        middleName=fake.first_name(),
-        lastName=fake.last_name()
-      )
-    teacher.password = "superadmin"
-    teacher.save()
-    superadmin = Admin(teacher_id=teacher.id)
-    superadmin.privileges = {
-                "create": True,
-                "delete": True,
-                "update": True,
-                "superadmin": True
-            }
+# def creatadmin():
+#     teacher = Teacher(
+#         username="superadmin",
+#         email="superadmin@school.com",
+#         firstName=fake.first_name(),
+#         middleName=fake.first_name(),
+#         lastName=fake.last_name()
+#       )
+#     teacher.password = "superadmin"
+#     teacher.save()
+#     superadmin = Admin(teacher_id=teacher.id)
+#     superadmin.privileges = {
+#                 "create": True,
+#                 "delete": True,
+#                 "update": True,
+#                 "superadmin": True
+#             }
 
-    superadmin.save()
+#     superadmin.save()
 
-    teacher = Teacher(
-        username="admin",
-        email="admin@school.com",
-        firstName=fake.first_name(),
-        middleName=fake.first_name(),
-        lastName=fake.last_name()
-      )
-    teacher.password = "admin"
-    teacher.save()
-    admin = Admin(teacher_id=teacher.id)
-    admin.privileges = {
-                "create": True,
-                "delete": True,
-                "update": True,
-                "superadmin": False
-    }
+#     teacher = Teacher(
+#         username="admin",
+#         email="admin@school.com",
+#         firstName=fake.first_name(),
+#         middleName=fake.first_name(),
+#         lastName=fake.last_name()
+#       )
+#     teacher.password = "admin"
+#     teacher.save()
+#     admin = Admin(teacher_id=teacher.id)
+#     admin.privileges = {
+#                 "create": True,
+#                 "delete": True,
+#                 "update": True,
+#                 "superadmin": False
+#     }
 
 
-    teacher = Teacher(
-        username="create",
-        email="create@school.com",
-        firstName=fake.first_name(),
-        middleName=fake.first_name(),
-        lastName=fake.last_name()
-      )
-    teacher.password = "create"
-    teacher.save()
-    admin = Admin(teacher_id=teacher.id)
-    admin.privileges = {
-                "create": True,
-                "delete": False,
-                "update": False,
-                "superadmin": False
-      }
-    admin.save()
+#     teacher = Teacher(
+#         username="create",
+#         email="create@school.com",
+#         firstName=fake.first_name(),
+#         middleName=fake.first_name(),
+#         lastName=fake.last_name()
+#       )
+#     teacher.password = "create"
+#     teacher.save()
+#     admin = Admin(teacher_id=teacher.id)
+#     admin.privileges = {
+#                 "create": True,
+#                 "delete": False,
+#                 "update": False,
+#                 "superadmin": False
+#       }
+#     admin.save()
 
-    print(GREEN + 
-          f"--{Admin.query.count()} USERS CREATED IN {database.upper()}--" + RESET)
+#     print(GREEN + 
+#           f"--{Admin.query.count()} USERS CREATED IN {database.upper()}--" + RESET)
 
-    print(GREEN + "ADNIN 1 LOGIN DETAILS" + RESET)
-    print(GREEN + "\t\tEmail: superadmin@school.com\n\t\tPassword: superadmin" + RESET)
+#     print(GREEN + "ADNIN 1 LOGIN DETAILS" + RESET)
+#     print(GREEN + "\t\tEmail: superadmin@school.com\n\t\tPassword: superadmin" + RESET)
 
-    print(GREEN + "ADNIN 2 LOGIN DETAILS" + RESET)
-    print(GREEN + "\t\tEmail: create@school.com\n\t\tPassword: create" + RESET)
+#     print(GREEN + "ADNIN 2 LOGIN DETAILS" + RESET)
+#     print(GREEN + "\t\tEmail: create@school.com\n\t\tPassword: create" + RESET)
 
-    student = Student(
-        username="student",
-        email="student@school.com",
-        firstName=fake.first_name(),
-        middleName=fake.first_name(),
-        lastName=fake.last_name()
-      )
-    student.password = "student"
-    student.admission_no = "78sa"
-    student.arm = "A"
-    student.save()
+#     student = Student(
+#         username="student",
+#         email="student@school.com",
+#         firstName=fake.first_name(),
+#         middleName=fake.first_name(),
+#         lastName=fake.last_name()
+#       )
+#     student.password = "student"
+#     student.admission_no = "78sa"
+#     student.arm = "A"
+#     student.save()
--- setups/setup.py
+++ setups/setup.py
@@ -7,3 +7,4 @@ from .admin import creatadmin
 # subprocess.run("cat setups/db_setup.sql | mariadb -uroot -proot", shell=True, capture_output=True, text=True)
 subprocess.run(["python3", "manage.py"], capture_output=True, text=True)
 subprocess.run(["python3", "-m", "setups.scripts"], capture_output=True, text=True)
+creatadmin()
