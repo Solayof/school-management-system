594022fe31b5a7c8e93485fb2332f17f6403664e
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-06-20 23:27:13 +0100

    update setup to create admin

--- setups/setup.py
+++ setups/setup.py
@@ -1,7 +1,10 @@
 #/usr/bin/python3
 import subprocess
 
+from .admin import creatadmin
+
 
 # subprocess.run("cat setups/db_setup.sql | mariadb -uroot -proot", shell=True, capture_output=True, text=True)
 subprocess.run(["python3", "manage.py"], capture_output=True, text=True)
 subprocess.run(["python3", "-m", "setups.scripts"], capture_output=True, text=True)
+creatadmin()
